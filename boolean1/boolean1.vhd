c684b63a1b195e7463aa0aa663824b6ed333e575
cirobr <ciro.rosa@alumni.usp.br>
2025-11-21 15:16:43 -0300

    upd

--- boolean1/boolean1.vhd
+++ boolean1/boolean1.vhd
@@ -3,14 +3,22 @@ use ieee.std_logic_1164.all;
 
 entity boolean1 is
     port (
-        A, B, C, D : in STD_LOGIC;
-        Y			 : out STD_LOGIC -- no comma
-    );
+		dp1, dp2, dp3, dp4 : in STD_LOGIC;
+		d1 : out STD_LOGIC
+	);
 end boolean1;
 
 
 architecture boolean1_arch of boolean1 is
+	signal a, b, c, d : STD_LOGIC;
+	signal y : STD_LOGIC;
 
 begin
-    Y <= (A and B) or (C and D);
+    y <= not ( (a and b) or (c and d) );
+
+	a <= dp1;
+	b <= dp2;
+	c <= dp3;
+	d <= dp4;
+	d1 <= not y;
 end boolean1_arch;
